15258b02b11a17e5b4df49febd0849c9c621289a
Bill Parrott <bill@chimericdream.com>
2023-11-30 01:39:31 -0600

    fix(logging): use the right directory for logging

--- service/src/logger.rs
+++ service/src/logger.rs
@@ -5,13 +5,18 @@ use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
 use log4rs::append::rolling_file::policy::compound::trigger::size::SizeTrigger;
 use log4rs::encode::pattern::PatternEncoder;
 use log4rs::config::{Appender, Config, Root};
+use crate::util::get_exe_dir;
 
 pub fn init() -> Result<(), Box<dyn std::error::Error>> {
+    let mut log_dir = get_exe_dir().unwrap();
+    log_dir.push("logs");
+    log_dir.push("output.log");
+
     let pattern = "[{d(%Y-%m-%d %H:%M:%S)}][{T:<12.12}][{l:<5.5}] {m}\n";
     let logfile = RollingFileAppender::builder()
         .encoder(Box::new(PatternEncoder::new(pattern)))
         .build(
-            "log/output.log",
+            log_dir.to_str().unwrap(),
             Box::new(
                 CompoundPolicy::new(
                     Box::new(SizeTrigger::new(1024 * 1024 * 10)),
