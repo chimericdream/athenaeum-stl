df36c0f4f0d9c3aa5e3692d510c3314d2acbb840
Bill Parrott <bill@chimericdream.com>
2023-12-16 23:20:14 -0600

    fix: make date sorting actually work

--- ui/src/hooks/models/use-model-list.ts
+++ ui/src/hooks/models/use-model-list.ts
@@ -1,8 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { useCallback, useMemo } from 'react';
+import { Temporal } from 'temporal-polyfill';
 
 import { useModelListContext } from '~/contexts/model-list-context';
 import { type Model, loadModels } from '~/services/athenaeum';
+import { rsToTemporal } from '~/util/dates';
 
 interface ModelListOverrides {
     mode?: 'grid' | 'list';
@@ -10,21 +12,32 @@ interface ModelListOverrides {
 }
 
 export const useModelList = (overrides?: ModelListOverrides) => {
-    const { mode, order } = useModelListContext();
+    const { mode, order, sort } = useModelListContext();
 
     const sortFunc = useCallback(
         (left: Model, right: Model) => {
-            const result = left.name.localeCompare(right.name, 'en-US', {
-                caseFirst: 'lower',
-                numeric: true,
-                sensitivity: 'base',
-            });
+            let result = 0;
+
+            if (sort === 'name') {
+                result = left.name.localeCompare(right.name, 'en-US', {
+                    caseFirst: 'lower',
+                    numeric: true,
+                    sensitivity: 'base',
+                });
+            }
+
+            if (sort === 'date') {
+                const lDate = rsToTemporal(left.imported_at);
+                const rDate = rsToTemporal(right.imported_at);
+
+                result = Temporal.Instant.compare(lDate, rDate);
+            }
 
             const sortOrder = overrides?.order ?? order;
 
             return sortOrder === 'asc' ? result : -1 * result;
         },
-        [order, overrides]
+        [order, overrides, sort]
     );
 
     const { data } = useQuery({ queryKey: ['models'], queryFn: loadModels });
--- ui/src/util/dates.ts
+++ ui/src/util/dates.ts
@@ -1,3 +1,9 @@
+import { Temporal } from 'temporal-polyfill';
+
 export const rsToJsDateString = (rsDate: string): string => {
     return `${rsDate.replaceAll(' ', 'T')}Z`;
+};
+
+export const rsToTemporal = (rsDate: string): Temporal.Instant => {
+    return Temporal.Instant.from(rsToJsDateString(rsDate));
 };
