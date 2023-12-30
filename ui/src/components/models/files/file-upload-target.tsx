f8d73a4a79cce540a04dfa0ae0c5b45e2f512193
Bill Parrott <bill@chimericdream.com>
2023-12-29 22:58:54 -0600

    chore: reduce throttle threshold for file uploads

--- ui/src/components/models/files/file-upload-target.tsx
+++ ui/src/components/models/files/file-upload-target.tsx
@@ -9,8 +9,8 @@ import queue from 'throttled-queue';
 
 import { type ModelRecord, uploadFileToModel } from '~/services/athenaeum';
 
-// Limit file uploads to 1 at a time, with a 1s delay between each
-const throttle = queue(1, 1000);
+// Limit file uploads to 1 at a time, with a 300ms delay between each
+const throttle = queue(1, 300);
 
 export const FileUploadTarget = ({ id }: { id: string }) => {
     const queryClient = useQueryClient();
